53a66b042f3cb27805224906cbedb109e8f87527
phfimmers <philippefimmers@gmail.com>
2020-10-06 16:46:31 +0200

    first submit

---
+++ main.py
@@ -0,0 +1,59 @@
+"""This file holds the logic for running a new game"""
+
+import random
+
+from IPython.display import clear_output
+
+import utils.game
+
+# start the first run of a game
+game_active = True
+run = 1
+while game_active:
+    # create a full deck of cards
+    card_symbols = ['♥', '♦', '♣', '♠']
+    card_numbers = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    mydeck = []
+    for symbol in card_symbols:
+        for number in card_numbers:
+            mydeck.append(utils.game.Card(symbol, number))
+    random.shuffle(mydeck)
+
+    # get player details
+    if run == 1:
+        player_names = ['']
+        while len(player_names) > 5 or len(player_names) == 1:
+            players_input = input("Please enter the player names separated by commas (max 5):")
+            player_names = players_input.split(sep=',')
+        else:
+            print(f"{len(player_names)} players registered; {','.join(player_names)}")
+
+    # create a board
+    myboard = utils.game.Board(player_names)
+
+    # create players with 10 cards each
+    players = [utils.game.Player(name=player_name, deck=mydeck, board=myboard) for player_name in player_names]
+
+    # play the game on semi-autopilot
+    cards_left_to_play = True
+    while cards_left_to_play:
+        for player in players:
+            if player.play() == "empty":
+                cards_left_to_play = False
+                break
+        else:
+            clear_output(wait=True)
+            print(myboard)
+    
+    # replay (reusing the same players, or stop)
+    
+    replay_answer = input("Game finished! Type 'y' if you want to go again.")
+    if replay_answer == 'y':
+        run += 1
+    else:
+        game_active = False
+else:
+    print("Card game stopped")
+
+
+
---
+++ utils/game.py
@@ -0,0 +1,93 @@
+"""This file defines the classes used by the card game logic in main.py"""
+
+from typing import List
+from typing import Union
+
+
+class Card():
+    """ Class defining one card by two arguments:
+    :symbol: one of ['♥', '♦', '♣', '♠']
+    :number: one of ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    """
+    
+    color_scheme = {'♥': 'red', '♦': 'red', '♣': 'black', '♠': 'black'}
+    
+    def __init__(self, symbol: str, number: Union(int, str)):
+        self.symbol = symbol
+        self.number = number
+        self.color = Card.color_scheme[symbol]
+    
+    def __str__(self):
+        print(f"{self.symbol}{self.number}\n")
+
+
+class Board():
+    """ Class defining a board particular to a card game:
+    it contains an active_cards list containing the 6 most
+    recent cards played, and an played_cards list
+    containing the other already played cards.
+    :players: the list of player names
+    """
+    
+    def __init__(self, player_names: List[str]):
+        self.players = player_names
+        self.active_cards = []
+        self.played_cards = []
+    
+    def __str__(self):
+        print("Active cards:\n")
+        for board_player in self.players:
+            print(f'{board_player}:')
+            for player, card in self.active_cards:
+                if board_player == player:
+                    print(card)
+            else:
+                print("\n")
+        print("Played cards:\n")
+        for board_player in self.players:
+            print(f'{board_player}:')
+            for player, card in self.played_cards:
+                if board_player == player:
+                    print(card)
+            else:
+                print("\n")
+    
+    def play_card(self, name: str, card):
+        """Receive the card from a player and register
+        it as a tuple into the FIFO active_cards list,
+        overflowing the 7th card onto played_cards"""
+        self.active_cards.append((name, card))
+        if len(self.active_cards) == 7:
+            self.played_cards.append(self.active_cards[0])
+            self.active_cards = self.active_cards[1:]
+
+
+class Player():
+    """ Class defining a player providing a name, a board
+    to play on, and a deck of cards from which the top 10 are
+    taken
+    :name:
+    :deck: 
+    """
+    
+    def __init__(self, name: str, deck, board):
+        self.name = name
+        self.board = board
+        self.cards = []
+        for i in range(9):
+            self.cards.append(deck.pop())
+    
+    def play(self):
+        """plays the top card or returns 'empty'"""
+        if len(self.cards) > 0:
+            self.board.play_card(name=self.name, card=self.cards.pop())
+            print(f'{self.name} played their card.\n')
+        else:
+            return "empty"
+    
+    def shuffle():
+        random.shuffle(self.cards)
+        print(f'{self.name} shuffled their cards.\n')
+
+    
+    
